6a423c606f9628d67956948c8fd185158ac9ba57
eggeek <eggeek.me@gmail.com>
2024-06-02 15:56:01 +0800

    update: expr script

--- run_expr.py
+++ run_expr.py
@@ -7,7 +7,7 @@ from math import sqrt
 from pathplot import readPath
 
 OK: int = 0
-TLM: float = 500.0 
+TLM: float = 500.0
 
 
 def genResJson(resfile: str, runtime: float) -> dict:
@@ -25,7 +25,7 @@ def genResJson(resfile: str, runtime: float) -> dict:
         lb += sqrt((sx - gx) ** 2 + (sy - gy) ** 2)
     res["lb"] = lb
     res["ub"] = ub
-    res['runtime'] = runtime
+    res["runtime"] = runtime
     res["paths"] = paths
     return res
 
@@ -33,35 +33,48 @@ def genResJson(resfile: str, runtime: float) -> dict:
 def run(scenfile: str, timelimit: float):
     dirname = os.path.dirname(scenfile)
     resfile = os.path.join(dirname, "kcbs.plan")
+    runtime = timelimit
+    resJsonPath = os.path.join(dirname, "kcbs.json")
+
     tstart = time.perf_counter()
-    code = os.system(f"./bin/expr {scenfile} {resfile} {timelimit}")
+    os.system(f"./bin/expr {scenfile} {resfile} {timelimit}")
     tend = time.perf_counter()
     runtime = tend - tstart
-    if code == OK and os.path.exists(resfile):
+
+    if os.path.exists(resfile):
         resDict = genResJson(resfile, runtime)
-        resJsonPath = os.path.join(dirname, "kcbs.json")
-        print (f"Saving result to {resJsonPath}", file=sys.stderr)
+        print(f"Saving result to {resJsonPath}")
         json.dump(resDict, open(resJsonPath, "w"), indent=2)
 
 
 def runall(dirname: str):
+    torun = []
     for scen in os.listdir(dirname):
         scenfile = os.path.join(dirname, scen, "data.scen")
-        if os.path.exists(scenfile):
-            print (f"Running scen {scenfile}", file=sys.stderr)
-            run(scenfile, TLM)
+        if not os.path.exists(scenfile):
+            print (f"Missing scen file [{scenfile}], skip")
+        resfile = os.path.join(dirname, scen, "kcbs.plan")
+        resJsonPath = os.path.join(dirname, scen, "kcbs.json")
+        print(f"Processing scen {scenfile}")
+        
+        # # run expr if no resfile or the resfile is not the right version
+        exists = os.path.exists(resJsonPath)
+        runtime = -1 if not exists else json.load(open(resJsonPath, 'r')).get('runtime', -1)
+        if not exists or runtime < TLM:
+            torun.append(scenfile)
+        else:
+            print (f"Existing result [{resJsonPath}] seems the right version, skip.")
+            continue
 
-            resfile = os.path.join(dirname, scen, "kcbs.plan")
-            if not os.path.exists(resfile):
-                print (f"No solution: {scenfile}", file=sys.stderr)
-                continue
-            # from pathplot import draw_sol
-            # print ("Generating animation ...")
-            # ani = draw_sol(scenfile, resfile, 20, 500)
-            # anifile = os.path.join(dirname, scen, "kcbs.gif")
-            # ani.save(anifile)
-            # print ("Done.")
+    print ("To run: ", '\n'.join([f"  {i}: [{task}]" for i, task in enumerate(torun)]))
 
+    for scenfile in torun:
+        print(f"Running scen {scenfile}")
+        run(scenfile, TLM)
+
+        if not os.path.exists(resfile):
+            print(f"No solution: {scenfile}")
+            continue
 
 
 if __name__ == "__main__":
