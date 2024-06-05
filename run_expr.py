49258ab27f6765fe997e17138a2b10bc9a2b28c7
eggeek <eggeek.me@gmail.com>
2024-06-05 21:18:29 +0800

    add: gen kcbs.json from kcbs.plan

--- run_expr.py
+++ run_expr.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import os
+import numpy as np
 import json
 import sys
 from math import sqrt
@@ -24,10 +25,18 @@ def readStatistic(resfile: str):
     return res
 
 
-def genResJson(resfile: str) -> dict:
+def genResJson(scenJson: str, resfile: str) -> dict:
+    print ("scen:", scenJson)
     res = readStatistic(resfile)
-    paths = readPath(resfile)
+
+    scens = json.load(open(scenJson, 'r'))['scens'][0]
     lb = 0
+    for i in range(len(scens['s_x'])):
+        sx, sy = scens['s_x'][i], scens['s_y'][i]
+        gx, gy = scens['d_x'][i], scens['d_y'][i]
+        lb += sqrt((sx - gx) ** 2 + (sy - gy) ** 2)
+
+    paths = readPath(resfile)
     ub = 0
     for path in paths.values():
         for i in range(len(path) - 1):
@@ -36,7 +45,6 @@ def genResJson(resfile: str) -> dict:
             ub += sqrt((cx - nx) ** 2 + (cy - ny) ** 2)
         sx, sy = path[0]
         gx, gy = path[-1]
-        lb += sqrt((sx - gx) ** 2 + (sy - gy) ** 2)
     res["lb"] = lb
     res["ub"] = ub
     res["paths"] = paths
@@ -50,7 +58,7 @@ def run(scenfile: str, timelimit: float):
 
     os.system(f"./bin/expr {scenfile} {resfile} {timelimit}")
     if os.path.exists(resfile):
-        resDict = genResJson(resfile)
+        resDict = genResJson(scenfile.removesuffix(".scen")+".json", resfile)
         print(f"Saving result to {resJsonPath}")
         json.dump(resDict, open(resJsonPath, "w"), indent=2)
 
@@ -58,7 +66,7 @@ def run(scenfile: str, timelimit: float):
 def runall(dirname: str):
     torun = []
     for scen in os.listdir(dirname):
-        scenfile = os.path.join(dirname, scen, "data.scen")
+        scenfile = os.path.join(dirname, scen, "data.json")
         if not os.path.exists(scenfile):
             print (f"Missing scen file [{scenfile}], skip")
         resfile = os.path.join(dirname, scen, "kcbs.plan")
@@ -74,7 +82,7 @@ def runall(dirname: str):
             print (f"Existing result [{resJsonPath}] seems the right version, skip.")
             continue
 
-    print ("To run: ", '\n'.join([f"  {i}: [{task}]" for i, task in enumerate(torun)]))
+    print ("To run:\n", '\n'.join([f"  {i}: [{task}]" for i, task in enumerate(torun)]))
 
     for scenfile in torun:
         resfile = os.path.join(os.path.dirname(scenfile), "kcbs.plan")
@@ -86,8 +94,46 @@ def runall(dirname: str):
             continue
 
 
+def genall(dirname: str):
+    to_gen_res = []
+    no_sol = []
+    for scen in os.listdir(dirname):
+        resfile = os.path.join(dirname, scen, "kcbs.plan")
+        jsonfile = os.path.join(dirname, scen, "kcbs.json")
+        if os.path.exists(resfile) and not os.path.exists(jsonfile):
+            to_gen_res.append(scen)
+        if not os.path.exists(resfile):
+            no_sol.append(scen)
+
+    print ("To gen json:\n", '\n'.join(to_gen_res))
+    for scen in to_gen_res:
+        scenfile = os.path.join(dirname, scen, "data.json")
+        resfile = os.path.join(dirname, scen, "kcbs.plan")
+        jsonfile = os.path.join(dirname, scen, "kcbs.json")
+        resDict = genResJson(scenfile, resfile)
+        json.dump(resDict, open(jsonfile, 'w'), indent=2)
+
+    print ("No sol:\n", '\n'.join(no_sol))
+    for scen in no_sol:
+        envfile = os.path.join(dirname, scen, "data.json")
+        jsonfile = os.path.join(dirname, scen, "kcbs.json")
+        scens = json.load(open(envfile, 'r'))['scens'][0]
+        nagent = len(scens['s_x'])
+        lb = 0
+        for i in range(nagent):
+            sx, sy = scens["s_x"][i], scens['s_y'][i]
+            gx, gy = scens["d_x"][i], scens['d_y'][i]
+            lb += sqrt((sx-gx)**2 + (sy-gy)**2)
+        resDict = dict(lb=lb, ub=np.inf, runtime=TLM, paths={})
+        json.dump(resDict, open(jsonfile, 'w'), indent=2)
+
+
+
+
 if __name__ == "__main__":
     if sys.argv[1] == "all":
         runall("./cbs-scen")
+    elif sys.argv[1] == "gen":
+        genall("./cbs-scen")
     else:
         run(sys.argv[1], TLM)
